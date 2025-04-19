c1dad7f83fcfca8505318f2c6b4a3f3116f8fcbd
Hilal Ahmad <115874624+hilalahmad0101@users.noreply.github.com>
2025-04-19 10:35:51 -0700

    feat: uupdate kubectl fixing

--- config-kubctl.sh
+++ config-kubctl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
-sudo mv kubectl /usr/local/bin/
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
